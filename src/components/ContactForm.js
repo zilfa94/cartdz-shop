a59706562a5a0013d76ec42a42c14c0d7e6b7b7c
fazil <fnahnah@gmail.com>
2022-07-12 21:35:39 +0100

    add contact form avec choix

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,7 +4,7 @@ import './ContactForm.css';
 function ContactForm() {
   return (
     <div className="contact">
-      <h2>Formulaire</h2>
+      <h2>Carte cadeau PlayStation pas cher - codes de cartes-cadeaux PSN</h2>
       <form
         name="contact"
         method="post"
@@ -12,6 +12,39 @@ function ContactForm() {
         data-netlify="true"
         onSubmit="submit"
       >
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox1"
+            value="option1"
+          />
+          <label className="form-check-label" for="inlineCheckbox1">
+            Carte PlayStation Network 10 EUR
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox2"
+            value="option2"
+          />
+          <label className="form-check-label" for="inlineCheckbox2">
+            Carte PlayStation Network 15 EUR
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox3"
+            value="option3"
+          />
+          <label className="form-check-label" for="inlineCheckbox3">
+            Carte PlayStation Network 20 EUR
+          </label>
+        </div>
         <input type="hidden" name="form-name" value="contact" />
         <label for="name">Nom: </label>
         <input type="text" name="name" />
