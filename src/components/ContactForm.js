872cff449a8916294712ffccb0ca250ee57ae9f7
fazil <fnahnah@gmail.com>
2022-07-12 13:24:39 +0100

    add contact form avec titre

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,6 +4,7 @@ import './ContactForm.css';
 function ContactForm() {
   return (
     <div className="contact">
+      <h2>Formulaire</h2>
       <form
         name="contact"
         method="post"
@@ -12,10 +13,10 @@ function ContactForm() {
         onSubmit="submit"
       >
         <input type="hidden" name="form-name" value="contact" />
-        <label for="name">Your Name: </label>
+        <label for="name">Nom: </label>
         <input type="text" name="name" />
 
-        <label for="email">Your Email: </label>
+        <label for="email">Email: </label>
         <input type="email" name="email" />
 
         <label for="message">Message: </label>
